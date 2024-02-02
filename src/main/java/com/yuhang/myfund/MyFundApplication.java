5e9906a39fcebe79deb6199326c0381d209faa8c
Yuhang Zhang <yuhang.september@gmail.com>
2024-02-02 10:43:49 -0500

    Initial the framework

---
+++ src/main/java/com/yuhang/myfund/MyFundApplication.java
@@ -0,0 +1,13 @@
+package com.yuhang.myfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuhang/myfund/MyFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhang.myfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
